45a435026550ccd5ec2975b9b05ea1fa03b486f6
codewithdaniyal <167603648+codewithdaniyal@users.noreply.github.com>
2024-06-04 18:25:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var guestList = ["Omair", "Junaid", "Daniyal"];
+for (var _i = 0, guestList_1 = guestList; _i < guestList_1.length; _i++) {
+    var elements = guestList_1[_i];
+    console.log("".concat(elements, ", we'd love to have you join us for dinner tonight!"));
+}
---
+++ app.ts
@@ -0,0 +1,4 @@
+let guestList: string[] = ["Omair", "Junaid", "Daniyal"];
+for(let elements of guestList){
+    console.log(`${elements}, we'd love to have you join us for dinner tonight!`)
+}
